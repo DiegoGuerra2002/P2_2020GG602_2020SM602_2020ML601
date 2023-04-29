9f0002383d6960d840e4592a0ecf507545de2d08
Diego <diego.guevara@catolica.edu.sv>
2023-04-29 08:06:16 -0600

    Agregar archivos de proyecto.

---
+++ P2_2020GG602_2020SM602_2020ML601/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P2_2020GG602_2020SM602_2020ML601.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
