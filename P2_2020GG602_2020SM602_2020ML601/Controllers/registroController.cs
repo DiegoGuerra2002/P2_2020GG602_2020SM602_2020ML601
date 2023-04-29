a12cb8eacb0938bd0005037e6e50d32029b62709
Henry <henry.solito@catolica.edu.sv>
2023-04-29 09:59:20 -0600

    registro Controller

--- P2_2020GG602_2020SM602_2020ML601/Controllers/registroController.cs
+++ P2_2020GG602_2020SM602_2020ML601/Controllers/registroController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using P2_2020GG602_2020SM602_2020ML601.Models;
 
 namespace P2_2020GG602_2020SM602_2020ML601.Controllers
 {
     public class registroController : Controller
     {
+        private readonly hospitalDbContext _hospitalDbContext;
+        public registroController(hospitalDbContext hospitalDbContext)
+        {
+            _hospitalDbContext = hospitalDbContext;
+        }
         public IActionResult Index()
         {
+            var listaDepartamentos = (from m in _hospitalDbContext.departamentos
+                                      select m).ToList();
+            ViewData["listaDepartamentos"] = new SelectList(listaDepartamentos, "iddepartamento", "nombredepartamento");
+
             return View();
         }
     }
